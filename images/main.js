7a17e40e7ef17700b6945cc4bdf35e32f09d900e
Vidhipriya <lvidhipriyapokiril@gmail.com>
2022-08-08 16:25:49 +0400

    updated main page

---
+++ images/main.js
@@ -0,0 +1,4 @@
+let countdownExample = document.getElementById('countdown-element');
+new Countdown(countdownExample, {
+  countdown: "31 December 2021 23:59:59",
+})
--- script.js
+++ script.js
@@ -1,20 +1,2 @@
 
-document.getElementById("login").addEventListener("click", validate);
- var attempt = 3;
-function validate(){ 
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-         if (( username == "aviramjeeawon@gmail.com" && password == "avi") || (username == "vidhipriya@gmail.com" && password == "vidhi")){
-             alert ("Login successfully");
-//window.location = "success.html"; // Redirecting to other page.
-         return false;}
-         else{
-               attempt --;
-             alert("You have "+attempt+" attempt left;");
-         if( attempt == 0){
-             document.getElementById("username").disabled = true;
-             document.getElementById("password").disabled = true;
-             document.getElementById("submit").disabled = true;
-          return false;}}}
-
 
